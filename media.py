54bd235bd16b5a934f58673713d56e193469270a
Chi Yang <nunuh89@gmail.com>
2017-02-10 23:05:19 -0800

    clean up and format

--- media.py
+++ media.py
@@ -1,6 +1,16 @@
 import webbrowser
 
 class Movie():
+    """
+    Movie Class
+    attributes:
+        title
+        storyline
+        poster_image_url
+        trailer_youtube_url
+    functions:
+        show_trailer
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -10,8 +20,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
-#toy = Movie("alkdf", "adskfjhklajsdhf", "adfhlaidhf", "https://www.youtube.com/watch?v=sHNp-SYTtiQ")
-
-#print(toy.storyline)
