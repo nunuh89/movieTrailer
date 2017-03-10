c07b375a48586e68c8342a8daffe3234dba37ccc
Chi Yang <nunuh89@gmail.com>
2017-03-10 14:05:32 -0800

    correct according to PEP8

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 class Movie():
     """
     Movie Class
@@ -11,12 +12,12 @@ class Movie():
     functions:
         show_trailer
     """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(
+            self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
