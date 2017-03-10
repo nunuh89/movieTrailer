626c2f1f8796a8e385b5f1303d747ada3332f983
Chi Yang <nunuh89@gmail.com>
2017-03-10 14:11:54 -0800

    replace shorturl for videos

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,40 +9,40 @@ la_la_land = media.Movie(
     "La La Land",
     "Musical drama about pursing dreams",
     "http://alturl.com/zv2j8",
-    "http://alturl.com/xpjhs")
+    "https://www.youtube.com/watch?v=0pdqf4P9MB8")
 
 the_lobster = media.Movie(
     "The Lobster",
     "David will be turned into an animal if fails"
     " to find true love within 45 days",
     "http://alturl.com/ob99y",
-    "http://alturl.com/y6hw9")
+    "https://www.youtube.com/watch?v=TR_NcqD-Gfs")
 
 hacksaw_ridge = media.Movie(
     "Hacksaw Ridge",
     "American hero soldier in WWII to fight on "
     "the front lines without a weapon",
     "http://alturl.com/sfzhk",
-    "http://alturl.com/m4oi4")
+    "https://www.youtube.com/watch?v=s2-1hz1juBI")
 
 arrival = media.Movie(
     "Arrival",
     "When mysterious spacecrafts arrive, linguist"
     " Louise must decode their language to save the world.",
     "http://alturl.com/vycfr",
-    "http://alturl.com/rifrq")
+    "https://www.youtube.com/watch?v=tFMo3UJ4B4g")
 
 zootopia = media.Movie(
     "Zootopia",
     "Judy becomes the first police officer of Zootopia",
     "http://alturl.com/ycq75",
-    "http://alturl.com/6kqrq")
+    "https://www.youtube.com/watch?v=jWM0ct-OLsM")
 
 deadpool = media.Movie(
     "Deadpool",
     "Deadpool hunts down the man who nearly destroyed his life",
     "http://alturl.com/if4vn",
-    "http://alturl.com/o327i")
+    "https://www.youtube.com/watch?v=9vN6DHB6bJc")
 
 movies = [la_la_land, the_lobster, hacksaw_ridge, arrival, zootopia, deadpool]
 
